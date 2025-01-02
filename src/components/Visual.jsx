e8299caf98483bc8b1fa4e6bb773239cc1af32bc
yc <do36421@naver.com>
2025-01-02 18:00:22 +0900

    Add: 메인페이지 뼈대 작업중

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -3,7 +3,18 @@ import '../styles/Visual.css'
 function Visual (){
   return(
     <section id="main-visual">
-      <div></div>
+      <article className='main-img-box'>
+        <div><img src="../../public/img/main-visual-02.jpg" alt="" /></div>
+      </article>
+
+      <article className='main-visual-text'>
+        <div>
+          <p>광화문광장 배경의 크리스마스 마켓</p>
+          <p>광화문 마켓</p>
+          <p>기간 2024.12.13 (금) ~ 2025.01.05 (일)</p>
+          <button>자세히 보기</button>
+        </div>
+      </article>
     </section>
   )
 
