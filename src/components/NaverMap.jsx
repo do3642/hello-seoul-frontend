fb8b06271d4a810151a498413dfb7f77d88310be
yeong <chlgkdud111@gmail.com>
2025-01-03 12:49:38 +0900

    Add : 네이버 지도 화면 전체에 뿌려줌

--- src/components/NaverMap.jsx
+++ src/components/NaverMap.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+
 function NaverMap() {
+  useEffect(() => {
+    var mapOptions = {
+      center: new naver.maps.LatLng(37.566535, 126.9779692),
+      zoom: 12
+    }
+
+    var map = new naver.maps.Map('main-map', mapOptions);
+  }, [])
 
   return(
     <>
-      <p>지도 들어갈 자리임</p>
+      <div id="main-map" style={{width: '100vw', height: '100vh'}}/>
     </>
   )
 }
