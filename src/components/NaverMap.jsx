1c68d0ecefce84adfdc41f03d1a0a59172c4409e
yeong <chlgkdud111@gmail.com>
2025-01-03 11:54:56 +0900

    Add : 컴포넌트 폴더, 페이지 폴더 생성. Add : 네이버 지도 컴포넌트와 지도 페이지 컴포넌트를 임시로 만들어 라우트 생성하여 연결

---
+++ src/components/NaverMap.jsx
@@ -0,0 +1,10 @@
+function NaverMap() {
+
+  return(
+    <>
+      <p>지도 들어갈 자리임</p>
+    </>
+  )
+}
+
+export default NaverMap;
---
+++ src/pages/Map.jsx
@@ -0,0 +1,12 @@
+import NaverMap from "../components/NaverMap";
+
+function Map() {
+
+  return (
+    <>
+      <NaverMap />
+    </>
+  )
+}
+
+export default Map;
