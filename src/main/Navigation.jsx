7b30a2bef05ee9dccb79701bba59d01d726d34a0
yc <do36421@naver.com>
2025-01-06 15:55:55 +0900

    Add: 네비게이트 주소 이동함수 추가

--- src/main/Navigation.jsx
+++ src/main/Navigation.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './css/Navigation.css';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 const Navigation = ({ contentName, link }) => {
     const { t } = useTranslation();
     const navItems = t('nav', { returnObjects: true })[0];
+    const navigate = useNavigate();
 
+    const handleNavigation = (path) => {
+      navigate(path);  // 해당 경로로 이동
+    };
   return (
     <nav className="navigation">
       <ul>
-        <li>
+        <li onClick={() => handleNavigation('/map')}>
           <p>{navItems.navMap} <i className="fa-solid fa-map"></i></p>
           <span>{navItems.page}<i className="fa-solid fa-caret-right"></i></span>
         </li>
