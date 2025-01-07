87cf5d6a564057c187a1113615bac80356fb6786
yc <do36421@naver.com>
2025-01-07 10:08:46 +0900

    Update: 줌기능개선(morph메소드로 바꿈)

--- src/map/utils/mapHover.js
+++ src/map/utils/mapHover.js
@@ -31,29 +31,25 @@ function MapHover(map, geoJson) {
     });
 
 
+    // 클릭 시 구로 확대
     map.data.addListener('click', function (e) {
       var clickedFeature = e.feature;
+
       var geometry = clickedFeature;
       var bounds = geometry.bounds;
+
       if (bounds) {
        
         var minPoint = bounds.getMin();
         var maxPoint = bounds.getMax();
-        console.log(minPoint)
-        console.log(maxPoint)
     
-        // LatLngBounds 객체 생성 시, 좌하단과 우상단 좌표를 올바르게 설정
         var latLngBounds = new naver.maps.LatLngBounds(
           new naver.maps.LatLng(minPoint.y, minPoint.x), // 좌하단 (위도, 경도 순서)
           new naver.maps.LatLng(maxPoint.y, maxPoint.x)  // 우상단 (위도, 경도 순서)
         );
     
-        // LatLngBounds에서 중심 좌표를 추출
         var center = latLngBounds.getCenter();
-    
-        // 줌을 설정한 후 중심으로 이동
-        map.setZoom(14);
-        map.panTo(center);
+        map.morph(center,14,'linear')
       }
     });
     
